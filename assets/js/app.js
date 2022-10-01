cb6b60773d63b69494647758878b3f2abef95aa5
Kapa1497 <112313204+Kapa1497@users.noreply.github.com>
2022-10-01 09:05:12 +0300

    one

---
+++ assets/js/app.js
@@ -0,0 +1,81 @@
+
+//Задача 1
+
+//let sm;
+
+// sm = +prompt(`Сторона квадрата`);
+
+//let a = sm; 
+//let P = 4 * a;
+//console.log(`Периметра квадрата: ${P}`);
+
+//Задача 2
+
+//let sm;
+
+  //sm = +prompt(`Введите длинну ребра куба:`);
+
+//let a = sm
+//let v = a * a * a; 
+//let s = 6 * (a * a);
+//console.log(`Объем куба: ${v}`);
+//console.log(`Площадь куба: ${s}`);
+
+// Задача 3
+
+// let sm;
+
+//   sm = +prompt(`введите радиус окружности:`);
+
+// let a = sm;
+// let b = 2;
+// let p = 3.14;
+// let c = b * p * a;
+// let s = p * (a *a);
+// console.log(`Длина окружности: ${c}`);
+// console.log(`Площадь окружности: ${s}`);
+
+//Задача 4
+
+// let weight;
+// let volume;
+
+// kilo = +prompt(`Масса`);
+// liter = +prompt(`Об'ем`);
+
+// let m = weight; 
+// let v = volume; 
+// let P = m * v;
+// console.log(`Плотность: ${P}`);
+
+//Задача 5
+
+// let residents;
+// let square;
+
+// residents = +prompt(`Количество жителей`);
+// square = +prompt(`Площадь государсва`);
+
+// let n = residents; 
+// let s = square; 
+// let P = n / s;
+// console.log(`Плотность населения: ${P}`);
+
+// Задача 6
+
+// let leg1;
+// let leg2;
+
+//   leg1 = +prompt(`Введите длинну катета а:`);
+//   leg2= +prompt(`Введите длинну катета b:`);
+
+// let a = leg1;
+// let b = leg2;
+// let c =  Math.sqrt(a**2 + b**2); 
+
+// console.log(`Гипотенуза: ${c}`);
+
+
+
+
+
